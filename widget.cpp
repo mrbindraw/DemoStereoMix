9275bd97b1583f885242dad3c51eb6506e301b3f
Andrew Bindraw <mrbindraw@gmail.com>
2025-05-23 13:06:36 +0300

    Refactoring: removed unnecessary cast

--- widget.cpp
+++ widget.cpp
@@ -122,7 +122,7 @@ void Widget::handleDeviceEnableOnToggled(bool checked)
     ui->rbtnPowerMgrContinue->setEnabled(checked);
     ui->rbtnPowerMgrDisable->setEnabled(checked);
 
-    if(!SysAudio::getInstance().setEndpointVisibility(getStereoMixDeviceId(), (int)checked)) // 0/1 - disable/enable StereoMix device
+    if(!SysAudio::getInstance().setEndpointVisibility(getStereoMixDeviceId(), checked)) // 0/1 - disable/enable StereoMix device
     {
         qDebug() << "!SysAudio::getInstance().setEndpointVisibility: " << Q_FUNC_INFO;
         return;
